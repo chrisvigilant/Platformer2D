3dd33414a906bef623d815ddb42d7ad2ca73e8df
LAPTOP-V2B7T6E9\Laptop <chrisvigilant@gmail.com>
2021-02-22 01:31:51 +0200

    Game Manager & UI Manager Implementation ver 1.0

---
+++ Platformer2D/Assets/3. Game Manager/Scripts/GameManager.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : Singleton<GameManager>
+{
+    //Keep  track what Level we are currently in
+    //Load and unload levels
+    //Keep track of the game state
+    //Generate other Persistent Systems
+
+    public GameObject[] SystemPrefabs;
+
+    private List<GameObject> _instancedSystemPrefabs;
+    List<AsyncOperation> _loadOperations;
+
+    private string _currentLevelName = string.Empty;
+
+    private void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+
+        _instancedSystemPrefabs = new List<GameObject>();
+        _loadOperations = new List<AsyncOperation>();
+
+        InstatiateSystemPrefabs();
+
+        LoadLevel("FirstLevel");
+    }
+
+    void OnLoadOperationcomplete(AsyncOperation ao)
+    {
+        Debug.Log("Load Complete.");
+        _loadOperations.Remove(ao);
+    }
+    void OnUnloadOperationcomplete(AsyncOperation ao)
+    {
+        Debug.Log("Unload Complete.");
+    }
+
+    void InstatiateSystemPrefabs()
+    {
+        GameObject prefabInstance;
+        for (int i =0; i <SystemPrefabs.Length; ++i)
+        {
+            prefabInstance = Instantiate(SystemPrefabs[i]);
+            _instancedSystemPrefabs.Add(prefabInstance);
+
+        }
+    }
+    public void LoadLevel(string levelName)
+    {
+        AsyncOperation ao = SceneManager.LoadSceneAsync(levelName, LoadSceneMode.Additive);
+        if (ao == null)
+        {
+            Debug.LogError("Unable to Load Level: " + levelName);
+            return;
+        }
+        ao.completed += OnLoadOperationcomplete;
+        _loadOperations.Add(ao);
+        _currentLevelName = levelName;
+    }
+    public void UnloadLevel(string levelName)
+    {
+        AsyncOperation ao = SceneManager.UnloadSceneAsync(levelName);
+        if (ao == null)
+        {
+            Debug.LogError("Unable to Unload Level: " + levelName);
+            return;
+        }
+        ao.completed += OnUnloadOperationcomplete;
+    }
+
+    protected override void OnDestroy()
+    {
+        base.OnDestroy();
+
+        for (int i =0; i < _instancedSystemPrefabs.Count; ++i)
+        {
+            Destroy(_instancedSystemPrefabs[i]);
+        }
+        _instancedSystemPrefabs.Clear();
+    }
+}
---
+++ Platformer2D/Assets/3. Game Manager/Scripts/Menus/MainMenu.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    // Track the Animation Component
+    //track Animation Clips for Fade in/out
+    //Functions that can receive animation events
+    //Function to play fade in/out animations
+
+    [SerializeField] private Animation _mainMenyAnimator;
+    [SerializeField] private AnimationClip _fadeOutAnimation;
+    [SerializeField] private AnimationClip _fadeInAnimation;
+
+    public void OnFadeOutComplete()
+    {
+        Debug.LogWarning("Fadeout Complete.");
+    }
+
+    public void OnFadeInComplete()
+    {
+        Debug.LogWarning("Fadein Complete.");
+        UIManager.Instance.SetDummyCameraActive(true);
+    }
+
+    public void FadeIn()
+    {
+        _mainMenyAnimator.Stop();
+        _mainMenyAnimator.clip = _fadeInAnimation;
+        _mainMenyAnimator.Play();
+    }
+
+    public void FadeOut()
+    {
+        UIManager.Instance.SetDummyCameraActive(false);
+
+        _mainMenyAnimator.Stop();
+        _mainMenyAnimator.clip = _fadeOutAnimation;
+        _mainMenyAnimator.Play();
+    }
+}
