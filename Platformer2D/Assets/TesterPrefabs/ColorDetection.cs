43d6a959f0446c30a548790119c0170883ac52f1
Asteline <stellamocha@hotmail.com>
2021-02-23 03:56:53 +0200

    Basic Level/Player - Basic Raycast Layer detection

---
+++ Platformer2D/Assets/TesterPrefabs/ColorDetection.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorDetection : MonoBehaviour
+{
+	Rigidbody2D rb;
+	public GameObject back;
+	public LayerMask bg;
+	private SpriteRenderer rend;
+	public static RaycastHit2D hit;
+
+	void Start(){
+		rend = gameObject.GetComponent<SpriteRenderer>();
+		//rend.color = new Color (1, 0, 0, 1);
+	}
+
+    void FixedUpdate()
+    {
+		//RaycastHit hit;
+		Ray ray = new Ray(transform.position, Vector3.forward);
+		Vector3 fwd = this.transform.TransformDirection(0, 0, 1f);
+		Debug.DrawRay(this.transform.position, fwd, Color.green);
+
+		if (Physics2D.GetRayIntersection(ray, 1f, bg)){
+			Debug.Log("Dis: Background!");
+		}
+
+    }
+}
---
+++ Platformer2D/Assets/TesterPrefabs/PlayerMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	public float movementSpeed;
+	public Rigidbody2D rb;
+
+	public float mx;
+	public float jumpForce = 20f;
+	public LayerMask groundLayers;
+	public Transform feet;
+
+	public void Update(){
+		mx = Input.GetAxisRaw("Horizontal");
+
+		if (Input.GetButtonDown("Jump") && isGrounded()){
+			Jump();
+		}
+	}
+
+	public void  FixedUpdate(){
+		Vector2 movement = new Vector2(mx * movementSpeed, rb.velocity.y);
+
+		rb.velocity = movement;
+	}
+
+	void Jump(){
+		Vector2 movement = new Vector2(rb.velocity.x, jumpForce);
+
+		rb.velocity = movement;
+	}
+
+	public bool isGrounded(){
+		Collider2D groundCheck = Physics2D.OverlapCircle(feet.position, 0.5f, groundLayers);
+
+		if (groundCheck != null){
+			return true;
+		}
+
+		return false;
+	}
+}
