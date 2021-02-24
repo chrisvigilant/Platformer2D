ea67458c926dffcca34f752b49e3c433f6a6d687
Asteline <stellamocha@hotmail.com>
2021-02-24 04:53:37 +0200

    Animator - Run, RunLeft, Jump - (Unflipped Animation)

--- Platformer2D/Assets/TesterPrefabs/PlayerMovement.cs
+++ Platformer2D/Assets/TesterPrefabs/PlayerMovement.cs
@@ -12,7 +12,20 @@ public class PlayerMovement : MonoBehaviour
 	public LayerMask groundLayers;
 	public Transform feet;
 
-	public void Update(){
+	public int running, running_left, idle, jumping;
+
+	Animator animator;
+	
+    private void Start()
+    {
+		animator = GetComponent<Animator>();
+		running = Animator.StringToHash("Run");
+		running_left = Animator.StringToHash("RunLeft");
+		//idle = Animator.StringToHash("Idle");
+		jumping = Animator.StringToHash("Jump");
+	}
+
+    public void Update(){
 		mx = Input.GetAxisRaw("Horizontal");
 
 		if (Input.GetButtonDown("Jump") && isGrounded()){
@@ -24,12 +37,28 @@ public class PlayerMovement : MonoBehaviour
 		Vector2 movement = new Vector2(mx * movementSpeed, rb.velocity.y);
 
 		rb.velocity = movement;
+
+		if (Input.GetKey("d")){
+			animator.SetBool(running, true);
+		}
+		else if (Input.GetKey("a"))
+		{
+			animator.SetBool(running_left, true);
+		}
+
+		if (rb.velocity.x == 0)
+		{
+			animator.SetBool(running, false);
+			animator.SetBool(running_left, false);
+		}
 	}
 
 	void Jump(){
 		Vector2 movement = new Vector2(rb.velocity.x, jumpForce);
 
 		rb.velocity = movement;
+
+		animator.SetTrigger(jumping);
 	}
 
 	public bool isGrounded(){
